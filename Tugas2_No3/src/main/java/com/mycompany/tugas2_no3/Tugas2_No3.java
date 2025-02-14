bbd9a38debc32d18747041327f4e3f5362c09e08
Nasywa Putri Ramadhina <nasywaputrir28@upi.edu>
2025-02-14 07:34:05 +0700

    Add files via upload

---
+++ Tugas2_No3/src/main/java/com/mycompany/tugas2_no3/Tugas2_No3.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tugas2_no3;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Tugas2_No3 {
+
+    public static void main(String[] args) {
+         // Mendapatkan kode ASCII dari karakter '4' dan '6'
+        int ascii4 = (int) '4';
+        int ascii6 = (int) '6';
+        
+        // Menjumlahkan kedua kode ASCII
+        int sum = ascii4 + ascii6;
+        
+        // Menampilkan hasil
+        System.out.println("Kode ASCII dari '4': " + ascii4);
+        System.out .println("Kode ASCII dari '6': " + ascii6);
+        System.out.println("Jumlah kode ASCII: " + sum);
+    }
+}
